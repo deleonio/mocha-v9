ed81441483609e275591ba3da122cf305d6601f7
Martin Oppitz <github@martinoppitz.com>
2021-07-20 22:07:32 +0200

    chore: initial commit v8

---
+++ .mocharc.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  extension: ["js", "jsx", "ts", "tsx"],
+  package: "./package.json",
+  reporter: "spec",
+  require: [
+    "esbuild-register",
+    "jsdom-global/register",
+    "mock-local-storage",
+    "esm",
+  ],
+  spec: [
+    "src/**/*.spec.*s*",
+    "src/**/*.test.*s*",
+    "tests/unit/**/*.spec.*s*",
+    "tests/unit/**/*.test.*s*",
+  ],
+  ui: "bdd",
+};
+
+// Source: https://github.com/mochajs/mocha/tree/master/example/config
